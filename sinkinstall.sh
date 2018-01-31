77f3be32f81a2f9cdcd7fc6075ed629ba9227baa
Bartek Moczulski <bmoczulski@barracuda.com>
2018-01-31 15:19:49 +0100

    fixed

--- sinkinstall.sh
+++ sinkinstall.sh
@@ -1,3 +1,3 @@
-apt-get install iperf iperf3
-cat "/usr/bin/iperf3 -sD" >> /etc/rc.local
-cat "/usr/bin/iperf -sD" >> /etc/rc.local
+apt-get -y install iperf iperf3
+echo "/usr/bin/iperf3 -sD" >> /etc/rc.local
+echo "/usr/bin/iperf -sD" >> /etc/rc.local
